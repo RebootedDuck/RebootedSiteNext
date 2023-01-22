c712eb52bb2f1da17dcd963cec8b165ef8f9796e
Rebooted Duck <88654965+RebootedDuck@users.noreply.github.com>
2023-01-22 17:00:57 +0000

    Continue Updating NavBar

--- src/pages/transredditarchive.js
+++ src/pages/transredditarchive.js
@@ -1,5 +1,18 @@
+import listyles from '../components/list.module.css'
+import Head from 'next/Head'
+
 export default function transredditarchive() {
     return (
-        <h1>Hi</h1>
+        <>
+            <Head>
+                <title>RTA</title>
+            </Head>
+            <h2>Reddit Transgender Sub-Reddit Archive - As many as possible</h2>
+            <ul>
+                <li className={listyles.listelement}><a className={listyles.link} href='https://reddit.com/r/egg_irl'>r/egg_irl</a></li>
+                <li className={listyles.listelement}><a className={listyles.link} href='https://reddit.com/r/egg_irl'>r/egg_irl</a></li>
+            </ul>
+        </>
     )
 }
+
